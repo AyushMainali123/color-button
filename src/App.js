81ebffbedb2f423865f5ea4fcc3d559a4541f648
AyushMainali123 <ayushmainali11@gmail.com>
2022-02-09 18:09:49 +0545

    Tested and coded button click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useMemo, useState } from 'react';
 import './App.css';
 
+const INITIAL_STATE = {
+  COLOR: 'red',
+}
+
 function App() {
+
+  const [color, setColor] = useState(INITIAL_STATE.COLOR);
+  const textColor = useMemo(() => color === "red" ? "blue" : "red", [color]);
+
+  
+  const handleClick = () => {
+    setColor('blue');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{ backgroundColor: color }} onClick={handleClick}>Change to { textColor}</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByRole("link", {name: /learn react/i});
-  expect(linkElement).toBeInTheDocument();
+
+  //Find an element with role="button" and the text "Change to blue"
+  const colorButton = screen.getByRole("button", { name: "Change to blue" })
+
+  //Expect the background color of the button to be red
+  expect(colorButton).toHaveStyle(`
+    background-color: red;
+  `)
+
+  //Click the button
+  fireEvent.click(colorButton)
+
+  //Expect the background color of the button to be blue
+  expect(colorButton).toHaveStyle(`
+    background-color: blue;
+  `)
+
+  //Expect the text of the button to be "Change to red"
+  expect(colorButton).toHaveTextContent("Change to red")
+
+});
+
+
+
+
+test('button turns blue when clicked', () => {
+
 });
